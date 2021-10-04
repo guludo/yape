12df1742d37dfeca26eebd37c8347931859b0674
Gustavo Sousa <gustavo.jo.sousa@gmail.com>
2021-10-04 18:21:49 -0300

    chore: license project under MPL 2.0

--- yape/walkproto.py
+++ yape/walkproto.py
@@ -1,3 +1,6 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 """
 This module defines event types and functions used by `Node._op_walk()` and
 `Node._get_node_descriptor()`.
